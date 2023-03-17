e24f6341fcc14c806aa9654617c8a6193451cd26
William Hoffman <120506096+windrh@users.noreply.github.com>
2023-03-17 09:34:22 -0400

    Update D-Flip-Flop.vhdl

--- D-Flip-Flop.vhdl
+++ D-Flip-Flop.vhdl
@@ -4,9 +4,29 @@ use ieee.std_logic_1164.all;
 entity d_flipflop is
 port( clk, d : in std_logic;
 		q 		 : out std_logic);
-end d_flipflopl
+end d_flipflop;
 
 architecture arc of d_flipflop is
 begin 
+
 	q <= d when rising_edge(clk);
+	
 end arc; 
+
+-- or you can use this 
+
+entity d_flipflop is
+port( clk, d : in std_logic;
+		q 		 : out std_logic);
+end d_flipflop;
+architecture arc of d_flipflop is
+begin 
+	
+	process(clk)
+	begin 
+		if rising_edge(clk) then 
+			q <= d;
+		end if;
+	end process;
+
+end arc;
