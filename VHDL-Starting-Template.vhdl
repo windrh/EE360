305ee823e16dff9ad2569dbb4b81529a58124d7a
William Hoffman <120506096+windrh@users.noreply.github.com>
2023-03-21 13:22:11 -0400

    Create VHDL-Starting-Template.vhdl

---
+++ VHDL-Starting-Template.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity name is
+port( variable1, varialbe2 : in std_logic;
+		             variable3 : out std_logic);
+end name;
+
+architecture arc of name is
+begin 
+
+	q <= //boolean expressions;
+	
+end arc; 
